2678d52bc9e5ec031601a6a87390d9bc6fc4b3c0
Roman Gusev <gusev.ry@gmail.com>
2015-09-20 20:07:20 -0700

    Add tests

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var User = require(__dirname + '/../models/user');
 var jsonParser = require('body-parser').json();
-// var handleError = require(__dirname + '/../lib/handleError');
 var ee = require(__dirname + '/../lib/userEvents');
+var httpBasic = require(__dirname + '/../lib/http_basic');
+var handleError = require(__dirname + '/../lib/handleError');
+var eatAuth = require(__dirname + '/../lib/eat_auth');
+
 
 var userRouter = module.exports = exports = express.Router();
-
+ 
 userRouter.post('/signup', jsonParser, function(req, res) {
   var newUser = new User();
   newUser.email = req.body.email;
   if (req.body.id) newUser.playerId = req.body.id;
   //saveUser event starts chain of events, generate password hash, save new user, return token.
   ee.emit('saveUser', newUser, req, res);
+});
+
+userRouter.get('/signin', httpBasic, function(req, res) {
+  User.findOne({email: req.auth.email}, function(err, user) {
+    if (err) return handleError.standart(err, res);
+    if (!user) return handleError.userDoesNotExistsError('User Does Not Exists', res);
+    //check password and return token
+    ee.emit('authenticate', user, req.auth.password, req, res);
+  });
 });
 
---
+++ test/auth_test.js
@@ -0,0 +1,56 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+var expect = chai.expect;
+process.env.MONGO_URL = 'mongodb://localhost/auth_test';
+require(__dirname + '/../server');
+var mongoose = require('mongoose');
+var User = require(__dirname + '/../models/user');
+
+describe('auth routes', function() {
+  after(function(done) {
+    mongoose.connection.db.dropDatabase(function() {
+      done();
+    });
+  });
+
+  it('should be able to create a user', function(done) {
+    chai.request('localhost:3000/api')
+    .post('/signup')
+    .send({email:'test1', password: 'foobar123'})
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.body.token).to.have.length.above(0);
+      done();
+    });
+  });
+
+  describe('User already in database', function() {
+    before(function(done) {
+      var user = new User();
+      user.email = 'test2';
+      user.generateHash('foobar123', function(err, res) {
+        if (err) throw err;
+        user.save(function(err, data) {
+          if (err) throw err;
+          user.generateToken(function(err, token) {
+            if (err) throw err;
+            this.token = token;
+            done();
+          }.bind(this));
+        }.bind(this));
+      }.bind(this));
+    });
+
+    it('should be able to signin', function(done) {
+      chai.request('localhost:3000/api')
+      .get('/signin')
+      .auth('test2', 'foobar123')
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res.body.token).to.have.length.above(0);
+        done();
+      });
+    });
+  });
+});
