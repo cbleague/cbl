d1dad273c76b4d8addfe21153aec52594e97678a
Kumji Park <kumji.p@gmail.com>
2015-09-21 12:11:29 -0700

    add models

---
+++ models/team.js
@@ -0,0 +1,44 @@
+'use strict';
+var mongoose = require('mongoose');
+var Player = require(__dirname + '/player');
+
+var teamSchema = new mongoose.Schema({
+
+	name: {type: String, unique: true, required: true},
+
+  division: {type: String, required: true},
+
+  season: {type: Number, required: true},
+
+  players: [{type: ObjectId, ref: 'Player'}],  // type: Schema.ObjectId
+
+	logo: String, // img url 
+
+	administrator: {name: {first: String, middle: String, second: String},
+									 email: String, phone: String},
+
+	captain: {name: {first: String, middle: String, second: String},
+						 email: String, phone: String},
+
+	coach: {name: {first: String, middle: String, second: String},
+					 email: String, phone: String},
+
+	photo: String,
+
+  win: Number,
+
+  loss: Number,
+
+  pld: Number,
+
+  basket: {score: Number, lost: Number},
+
+  pts: Number,
+
+  adminChecked: {type: Boolean, default: false}
+
+}, {
+  collection: 'season' + season //season collection
+});
+
+module.exports = exports = mongoose.model('Team', teamSchema);
