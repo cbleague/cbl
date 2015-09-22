0777021896216423b931725cf8fae3f68af51683
Phil <philson3@gmail.com>
2015-09-21 21:10:31 -0700

    working on tests for team

---
+++ test/teams_test.js
@@ -0,0 +1,27 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+var expect = chai.expect;
+process.env.MONGO_URL = 'mongodb://localhost/team_test';
+require(__dirname + '/../server');
+var mongoose = require('mongoose');
+var Team = require(__dirname + '/../models/team');
+
+describe('team routes', function(){
+  after(function(done){
+    mongoose.connection.db.dropDatabase(function(){
+      done();
+    });
+  });
+
+  it('should be able to save a team', function(done){
+    chai.request('localhost:3000/api/register')
+    .post('/registerteam')
+    .send({name: 'Timberwolves  ', division: 'minor', season: 'winter'})
+    .end(function(err, res){
+      expect(err).to.eql(null);
+      expect(res.body.team).to.be.eql('Timberwolves');
+    });
+  });
+
+})
