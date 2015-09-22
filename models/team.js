4e341bf95dbb05d07a9e1237769cc91d491c2697
Roman Gusev <gusev.ry@gmail.com>
2015-09-22 16:49:23 -0700

    Working Season

---
+++ models/team.js
@@ -0,0 +1,27 @@
+'use strict';
+var mongoose = require('mongoose');
+//var Player = require(__dirname + '/player');
+var ObjectId = mongoose.Schema.Types.ObjectId
+var uniqueValidator = require('mongoose-unique-validator');
+
+var teamSchema = new mongoose.Schema({
+  name: {type: String, unique: true, required: true},
+  division: {type: String, required: true},
+  season: {type: String, required: true},
+  players: [{type: ObjectId, ref: 'Player'}],  // type: Schema.ObjectId
+  logo: String, // img url 
+  administrator: String,
+  captain: String,
+  coach: String,
+  photo: String,
+  win: Number,
+  loss: Number,
+  pld: Number,
+  basket: {score: Number, lost: Number},
+  pts: Number,
+  adminChecked: {type: Boolean, default: false}
+});
+
+teamSchema.plugin(uniqueValidator);
+
+module.exports = exports = mongoose.model('Team', teamSchema);
