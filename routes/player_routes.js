d7a9deee72a597401f2b3238e9c8f253476bf9e5
Kumji Park <kumji.p@gmail.com>
2015-09-21 20:59:46 -0700

    error checking for player

--- routes/player_routes.js
+++ routes/player_routes.js
@@ -4,11 +4,11 @@ var handleError = require(__dirname + '/../lib/handleError');
 
 var playerRouter = module.exports = exports = express.Router();
 
-playerRouter.post('/api/registerPlayer', function(req, res) {
+playerRouter.post('/registerPlayer', function(req, res) {
   var newPlayer = new Players();
-  newPlayer.name.first = req.body.first;
-  newPlayer.name.middle = req.body.middle;
-  newPlayer.name.second = req.body.second;
+  newPlayer.name.first = req.body.name.first;
+  newPlayer.name.middle = req.body.name.middle;
+  newPlayer.name.second = req.body.name.second;
   newPlayer.email = req.body.email;
   newPlayer.phone = req.body.phone;
   newPlayer.dateOfBirth = req.body.dateOfBirth;
@@ -18,11 +18,15 @@ playerRouter.post('/api/registerPlayer', function(req, res) {
   newPlayer.number = req.body.number;
   newPlayer.position = req.body.position;
   newPlayer.photo = req.body.photo;
+  newPlayer.save(function(err, data) {
+    if(err) handleError(err,res);
+    res.json(data);
+  });
 });
 
 playerRouter.get('/api/player/:email', function(req, res) {
   Player.findOne({'email': req.params.email}, function(err, player) {
-    if (err) return handleError(err, res);
+    if (err) handleError(err, res);
     console.log(player);
   });
 });
