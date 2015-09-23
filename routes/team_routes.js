435f3efa2e61fe033ae22d93c965bd5b731f1890
Phil <philson3@gmail.com>
2015-09-22 17:44:31 -0700

    team register debugging

--- routes/team_routes.js
+++ routes/team_routes.js
@@ -13,7 +13,8 @@ require(__dirname + '/../lib/teamEvents');
 
 var teamRouter = module.exports = exports = express.Router();
 
-teamRouter.post('/registerteam', isUser, jsonParser, function(req, res){
+teamRouter.post('/registerteam', jsonParser, isUser, function(req, res){
+  debugger;
   var newTeam = new Team();
   newTeam.name = req.body.name; 
   newTeam.division = req.body.division;
--- test/teams_test.js
+++ test/teams_test.js
@@ -6,23 +6,50 @@ process.env.MONGO_URL = 'mongodb://localhost/team_test';
 require(__dirname + '/../server');
 var mongoose = require('mongoose');
 var Team = require(__dirname + '/../models/team');
+var User = require(__dirname + '/../models/user');
+
+
+
 
 describe('team routes', function(){
+  
   after(function(done){
     mongoose.connection.db.dropDatabase(function(){
       done();
     });
   });
 
+  describe('user for team', function(){
+    before(function(done) {
+      var user = new User();
+      user.email = 'test2';
+      user.generateHash('foobar123', function(err, res) {
+        if (err) throw err;
+        user.save(function(err, data) {
+          if (err) throw err;
+          user.generateToken(function(err, token) {
+            if (err) throw err;
+            this.token = token;
+            done();
+          }.bind(this));
+        }.bind(this));
+      }.bind(this));
+    });
+
+  
+
   it('should be able to save a team', function(done){
+    var token = this.token;
     chai.request('localhost:3000/api/register')
     .post('/registerteam')
+    .set('token', token)
     .send({name: 'Timberwolves', division: 'minor', season: 'winter'})
     .end(function(err, res){
       expect(err).to.eql(null);
+      debugger;
       expect(res.body.name).to.eql('Timberwolves');
       done();
     });
   });
-
-})
+});
+});
