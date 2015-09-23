a1bcb24d838df0e84583828a4927a29b248d8fc6
Roman Gusev <gusev.ry@gmail.com>
2015-09-23 12:47:46 -0700

    Working on Season Tests

--- routes/season_routes.js
+++ routes/season_routes.js
@@ -3,10 +3,11 @@ var Season = require(__dirname + '/../models/season');
 var jsonParser = require('body-parser').json();
 var isAdmin = require(__dirname + '/../lib/eat_authorize');
 var handleError = require(__dirname + '/../lib/handleError');
+var ee = require(__dirname + '/../lib/seasonEvents');
 
 var seasonRouter = module.exports = exports = express.Router();
 
-//should receive only season name
+//should receive seasonNumber and seasonName
 seasonRouter.post('/', jsonParser, isAdmin, function(req, res) {
   var season = new Season();
   season.name = req.body.name;
@@ -15,4 +16,9 @@ seasonRouter.post('/', jsonParser, isAdmin, function(req, res) {
     if (err) return handleError.standard(err, res);
     res.json({msg: 'New Season Created'});
   });
+});
+
+//should receive teamId and seasonId
+seasonRouter.post('/addteam', jsonParser, isAdmin, function(req, res) {
+  ee.emit('addTeamToSeason', req, res);
 });
--- test/season_test.js
+++ test/season_test.js
@@ -8,14 +8,15 @@ var mongoose = require('mongoose');
 var Season = require(__dirname + '/../models/season');
 var User = require(__dirname + '/../models/user');
 
-describe('Season Routes Testing', function(){
+describe('Season Routes Testing', function() {
   after(function(done){
-    mongoose.connection.db.dropDatabase(function()  {
+    mongoose.connection.db.dropDatabase(function() {
       done();
     });
   });
 
-  describe('Create user with admin rights', function() {
+  describe('Creates a user and a team inorder to manipulate with season', function() {
+    
     before(function(done) {
       var user = new User();
       user.email = 'test';
@@ -33,22 +34,35 @@ describe('Season Routes Testing', function(){
       }.bind(this));
     });
 
-    describe('Test create season route', function() {
-      it('should be able to create season', function(done) {
-        var token = this.token;
-        chai.request('localhost:3000/api/season')
-        .post('/')
-        .set('token', token)
-        .send({seasonNumber: 9, name: '2014-2015'})
-        .end(function(err, res) {
-          Season.findOne({seasonNumber: 9}, function(err, season) {
-            expect(err).to.eql(null);
-            expect(season).to.exist;
-            expect(season.name).to.eql('2014-2015');
-            done();
-          });
+    it('Creates a team', function(done) {
+
+    })
+
+  
+  });
+
+
+  //creates user with admin role for creating season
+  
+  
+  describe('Test create season route', function() {
+    it('should be able to create season', function(done) {
+      var token = this.token;
+      chai.request('localhost:3000/api/season')
+      .post('/')
+      .set('token', token)
+      .send({seasonNumber: 9, name: '2014-2015'})
+      .end(function(err, res) {
+        Season.findOne({seasonNumber: 9}, function(err, season) {
+          expect(err).to.eql(null);
+          expect(season).to.exist;
+          expect(season.name).to.eql('2014-2015');
+          done();
         });
       });
     });
   });
+
+
+
 });
