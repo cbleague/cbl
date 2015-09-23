8c592c1763b7775c822da8669e659152cd0a01dc
Phil <philson3@gmail.com>
2015-09-22 21:43:55 -0700

    n

--- test/teams_test.js
+++ test/teams_test.js
@@ -12,44 +12,40 @@ var User = require(__dirname + '/../models/user');
 
 
 describe('team routes', function(){
-  
   after(function(done){
     mongoose.connection.db.dropDatabase(function(){
       done();
     });
   });
-
+  
   describe('user for team', function(){
-    before(function(done) {
+      before(function(done) {
       var user = new User();
-      user.email = 'test2';
+      user.email = 'test99';
       user.generateHash('foobar123', function(err, res) {
         if (err) throw err;
         user.save(function(err, data) {
           if (err) throw err;
           user.generateToken(function(err, token) {
             if (err) throw err;
-            this.token = token;
+             var token = this.token;
+             console.log(token);
             done();
           }.bind(this));
         }.bind(this));
       }.bind(this));
     });
-
-  
-
-  it('should be able to save a team', function(done){
-    var token = this.token;
-    chai.request('localhost:3000/api/register')
-    .post('/registerteam')
-    .set('token', token)
-    .send({name: 'Timberwolves', division: 'minor', season: 'winter'})
-    .end(function(err, res){
-      expect(err).to.eql(null);
-      debugger;
-      expect(res.body.name).to.eql('Timberwolves');
-      done();
+    
+    it('should be able to save a team', function(done){
+      chai.request('localhost:3000/api/team')
+      .post('/registerteam')
+      .set('token', token)
+      .send({name: 'Timberwolves', division: 'minor', season: 'winter'})
+      .end(function(err, res){
+        expect(err).to.eql(null);
+        expect(res.body.name).to.eql('Timberwolves');
+        done();
+      });
     });
   });
-});
 });
