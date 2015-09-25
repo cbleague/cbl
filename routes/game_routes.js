b59501157f825bf731122dd94f8b8c68335fa457
Phil <philson3@gmail.com>
2015-09-24 23:37:42 -0700

    readme added

--- routes/game_routes.js
+++ routes/game_routes.js
@@ -11,12 +11,8 @@ var gameRouter = module.exports = exports = express.Router();
 
 //create Game with req.seasonNumber, req.team1.name, req.team1.division, 
 //req.team2.division, req.team2.name, req.date, req.location
-gameRouter.post('/create', jsonParser, /*isAdmin,*/ function(req, res, err){
-  // Team.findOne({name:req.body.team1_name, division:req.body.team1_division}, function(err, team1){
-  //   Team.findOne({name:req.body.team2_name, division:req.body.team2_division}, function(err, team2){
-  //     if(team1 && team2) console.log('two teams exist');  
-  //   });
-  // });
+gameRouter.post('/create', jsonParser, isAdmin, function(req, res, err){
+
 
   gameEvents.emit('verifyTeam', req,res,err);
 });
@@ -24,7 +20,7 @@ gameRouter.post('/create', jsonParser, /*isAdmin,*/ function(req, res, err){
 
 //It will be an array of game in the day.
 //get Game with req.seasonNumber, req.division, req.date
-gameRouter.get('/find', jsonParser, /*isAdmin,*/ function(req, res){
+gameRouter.get('/find', jsonParser, isAdmin, function(req, res){
   gameEvents.emit('findGame', req, res, err);
 });
 
