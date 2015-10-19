005a731670730862fc854c0de66707480a1b1a1d
David Sheffield <david.sheffield21@gmail.com>
2015-10-19 15:05:54 -0700

    partial code added to addteam controller

--- src/app/admin/controllers/addTeam_controller.js
+++ src/app/admin/controllers/addTeam_controller.js
@@ -1,3 +1,10 @@
 module.exports = function(app){
-  app.controller('AddTeamController', []);
+  app.controller('AddTeamController', ['$scope', 'Resource', '$http', '$location', function($scope, Resource, $http, $location){
+
+  $scope.team = {};
+  $scope.newTeam = {};
+  var teamResource = Resource('team');
+
+
+  }]);
 };
