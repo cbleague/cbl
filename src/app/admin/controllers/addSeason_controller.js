db61cf2fa00f03056d0fdc10d5bb12abd49b1636
David Sheffield <david.sheffield21@gmail.com>
2015-10-19 15:37:56 -0700

    updating add season controller and router.js

--- src/app/admin/controllers/addSeason_controller.js
+++ src/app/admin/controllers/addSeason_controller.js
@@ -1,21 +1,9 @@
 module.exports = function(app){
-  app.controller('AddSeasonController', ['$scope', 'Resource', '$http', '$location', '$cookies', function($scope, Resource, $http, $location, $cookies){
+  app.controller('AddSeasonController', ['$scope', 'Resource', function($scope, Resource){
 
-    var eat = $cookies.get('eat', {secure: true});
-    if (!($cookies.get('eat').length))
-      $location.path('/signup');
-
-    $http.defaults.headers.common.token = eat;
     $scope.season = [];
     $scope.newSeason = {};
     var seasonResource = Resource('season'); // may need to change this
-
-    $scope.getAll = function(){
-      seasonResource.getAll(function(err, data){
-        if (err) return console.log('AddSeasonController getAll err ' + err);
-        $scope.season = data;
-      });
-    };
 
     $scope.createSeason = function(){
       seasonResource.create(season, function(err, data){
@@ -28,6 +16,7 @@ module.exports = function(app){
       });
     };
 
+    // need to add server routes for future functionality
     $scope.updateSeason = function(){
       seasonResource.update(season, function(err){
         season.editing = false;
@@ -35,6 +24,7 @@ module.exports = function(app){
       });
     };
 
+    // need to add server routes for future functionality
     $scope.removeSeason = function(){
       seasonResource.remove(season, function(err){
         if (err) return console.log('AddSeasonController remove err ' + err);
