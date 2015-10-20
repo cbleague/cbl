5588a252cc74faeead2c550643d815a033d96108
David Sheffield <david.sheffield21@gmail.com>
2015-10-20 16:48:43 -0700

    adding serve back to gulp

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['sass', 'fonts', 'index', 'html', 'webpack', 'watch']);
+gulp.task('default', ['sass', 'fonts', 'index', 'html', 'webpack', 'watch', 'serve']);
