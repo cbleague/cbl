830e47116f69b6f7ef19106d108e7a7208b3dfbc
David Sheffield <david.sheffield21@gmail.com>
2015-10-21 10:55:19 -0700

    adding add team to season to add team controller

--- src/app/admin/controllers/addTeam_controller.js
+++ src/app/admin/controllers/addTeam_controller.js
@@ -5,26 +5,41 @@ module.exports = function(app){
   $scope.newTeam = {};
 
   $scope.getSome = function(){
-      $http({
-        method: 'POST',
-        url: 'api/team/getteamsnotinseason/:season',
-        headers: {
-          'token': $cookies.get('token')
-        },
-        data: {
-          'seasonNumber': 10,
-          'inSeason': false
-        }
-      }).then(function(res){
-        $scope.teamArray = res.data;
-        console.log('we got data ' + res.data);
-      }, function(res){
-        console.log('AddTeamController getSome error ' + res);
-      });
-    };
+    $http({
+      method: 'POST',
+      url: 'api/team/getteamsnotinseason/:season',
+      headers: {
+        'token': $cookies.get('token')
+      },
+      data: {
+        'seasonNumber': 10,
+        'inSeason': false
+      }
+    }).then(function(res){
+      $scope.teamArray = res.data;
+      console.log('we got data ' + res.data);
+    }, function(res){
+      console.log('AddTeamController getSome error ' + res);
+    });
+  };
 
   $scope.addTeamToSeason = function(){
-
+    $http({
+      method: 'POST',
+      url: 'api/team/getteamsnotinseason/:season',
+      headers: {
+        'token': $cookies.get('token')
+      },
+      data: {
+        'seasonNumber': 10,
+        'inSeason': false
+      }
+    }).then(function(res){
+      $scope.teamArray = res.data;
+      console.log('we got data ' + res.data);
+    }, function(res){
+      console.log('AddTeamController getSome error ' + res);
+    });
   };
 
   $scope.updateTeam = function(){
