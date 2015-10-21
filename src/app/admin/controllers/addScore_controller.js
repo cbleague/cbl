6dbcf809809c02c6f21e03dbf7887ae318972dd8
David Sheffield <david.sheffield21@gmail.com>
2015-10-21 14:21:32 -0700

    drying out controllers to set headers token with .get

--- src/app/admin/controllers/addScore_controller.js
+++ src/app/admin/controllers/addScore_controller.js
@@ -2,14 +2,12 @@ module.exports = function(app){
   app.controller('AddScoreController', ['$scope', '$http', '$cookies', function($scope, $http, $cookies){
 
   $scope.score = {};
+  $http.defaults.headers.common.token = $cookies.get('token');
 
   $scope.addScore = function(){
     $http({
         method: 'POST',
         url: 'api/score/addscore',
-        headers: {
-          'token': $cookies.get('token')
-        },
         data: $scope.season
       }).then(function(res){
          console.log(res);
@@ -23,9 +21,6 @@ module.exports = function(app){
     $http({
       method: 'PUT',
       url: 'api/score/updatescore', // need to add this route
-      headers: {
-        'token': $cookies.get('token')
-      },
       data: {
         // grab by game and/or teams and date
       }
--- src/app/admin/controllers/addSeason_controller.js
+++ src/app/admin/controllers/addSeason_controller.js
@@ -6,9 +6,8 @@ module.exports = function(app){
       'seasonName': ''
     };
     $scope.season = {};
-    $scope.season.admin = {};
-    $scope.season.creator = $scope.loggedUser;
     $scope.seasons = [];
+    $http.defaults.headers.common.token = $cookies.get('token');
 
     // need to change the old seaon current to false
     $scope.createSeason = function(){
@@ -16,9 +15,6 @@ module.exports = function(app){
       $http({
         method: 'POST',
         url: 'api/season/createseason',
-        headers: {
-          'token': $cookies.get('token')
-        },
         data: $scope.season
       }).then(function(res){
          console.log(res);
@@ -33,9 +29,6 @@ module.exports = function(app){
      $http({
         method: 'PUT',
         url: 'api/season/updateseason',
-        headers: {
-          'token': $cookies.get('token')
-        },
         data: $scope.season
       }).then(function(res){
         $scope.season._id = res.data;
@@ -49,9 +42,6 @@ module.exports = function(app){
       $http({
         method: 'DELETE',
         url: 'api/season/deleteseason',
-        headers: {
-          'token': $cookies.get('token')
-        },
         data: $scope.season
       }).then(function(res){
         // remove what is already in MongoDB
